2a5971832584fe5a360f19bab269f84b87e0768b
Danilo Romão <htpps.sw@gmail.com>
2024-09-07 13:01:52 -0300

    commit initial

---
+++ src/lanchonete/area/cliente/Cliente.java
@@ -0,0 +1,17 @@
+package lanchonete.area.cliente;
+
+public class Cliente {
+
+     public void escolherLanche () {
+          System.out.println("Escolhendo lanche do menu");
+     }
+     public void fazerPedido () {
+          System.out.println("Fazendo o pedido do lanche!");
+     }
+     public void pagarConta () {
+          System.out.println("Pagando a conta");
+     }
+     private void consultarSaldoNoAplicativo () {
+          System.out.println("Verificando o saldo no aplicativo do banco");
+     }
+}
---
+++ src/lanchonete/atendimento/cozinha/Almoxarife.java
@@ -0,0 +1,19 @@
+package lanchonete.atendimento.cozinha;
+
+public class Almoxarife {
+     
+     private void controlarEntradaItem() {
+          System.out.println("Controlando a entrada de itens na lanchonete");
+     }
+     private void controlarSaidaItem() {
+          System.out.println("Controlando a sada de itens na lanchonete");
+     }
+     
+     void entregarIngredientes() {
+          System.out.println("Entregando ingredientes pedidos!");
+     }
+     
+     void trocarGas() {
+          System.out.println("Trocando o gas!");
+     }
+}
